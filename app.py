346f10bf144c755bb4e270961891547746f3c5d0
bryanathll <bryannatanael77@gmail.com>
2024-02-09 01:24:26 +0700

    InitialCommit

---
+++ app.py
@@ -0,0 +1,63 @@
+import os
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd 
+from  google_play_scraper import Sort, reviews
+from transformers import pipeline
+
+pretrained_name = "w11wo/indonesian-roberta-base-sentiment-classifier"
+
+nlp = pipeline(
+    "sentiment-analysis",
+    model=pretrained_name,
+    tokenizer=pretrained_name
+)
+
+app = Flask(__name__)
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        package_name = request.form["package_name"]
+        result, continuation_token = reviews(
+            package_name,
+            lang='id',
+            country='id',
+            count=100,
+            filter_score_with=None,
+            sort=Sort.NEWEST
+        )
+        df = pd.DataFrame(np.array(result), columns=['review'])
+
+        df=df.join(pd.DataFrame(df.pop('review').tolist()))
+
+        
+        df.rename(columns={'userName': 'Username', 'content': 'Review', 'score': 'Rating', 'userImage': 'User Image'}, inplace=True)
+        df = df[['User Image', 'Username', 'Review', 'Rating']]
+
+
+        df['User Image'] = df['User Image'].apply(
+        lambda x: f'<img src="{x}" width="50" height="50">')
+    
+        table_classes = 'table table-responsive table-stripped'
+
+        df_html = df.to_html(index=False, escape=False, classes=table_classes)
+
+        return render_template('index.html', table=df_html, package_name=package_name)
+    
+    else:
+        return render_template('index.html')
+
+@app.route("/api/predict", methods='POST')
+def prediction():
+    if request.method == "POST":
+        pass
+    else:
+        return "jsonify"({
+            "status":{
+                "code":405,
+                "message": "Method not allowed"
+            },"data" : None
+        }), 405
+
+if __name__ =="__main__":
+    app.run()
