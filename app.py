06c2f5b62c481fa29e3d15754436e93785245eb9
bryanathll <bryannatanael77@gmail.com>
2024-02-09 21:39:38 +0700

    InitialCommit

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 import os
-<<<<<<< HEAD
-from flask import Flask, request, render_template, jsonify
-import numpy as np
-import pandas as pd 
-from  google_play_scraper import Sort, reviews
-from transformers import pipeline
-
-=======
 
 import numpy as np
 import pandas as pd
@@ -17,7 +9,6 @@ from transformers import pipeline
 
 app = Flask(__name__)
 
->>>>>>> 474c780 (InitialCommit)
 pretrained_name = "w11wo/indonesian-roberta-base-sentiment-classifier"
 
 nlp = pipeline(
@@ -26,10 +17,6 @@ nlp = pipeline(
     tokenizer=pretrained_name
 )
 
-<<<<<<< HEAD
-app = Flask(__name__)
-=======
->>>>>>> 474c780 (InitialCommit)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -41,24 +28,6 @@ def index():
             country='id',
             count=100,
             filter_score_with=None,
-<<<<<<< HEAD
-            sort=Sort.NEWEST
-        )
-        df = pd.DataFrame(np.array(result), columns=['review'])
-
-        df=df.join(pd.DataFrame(df.pop('review').tolist()))
-
-        
-        df.rename(columns={'userName': 'Username', 'content': 'Review', 
-                           'score': 'Rating', 'userImage': 'User Image'}, inplace=True)
-        
-        df = df[['User Image', 'Username', 'Review', 'Rating']]
-
-
-        df['User Image'] = df['User Image'].apply
-        (lambda x: f'<img src="{x}" width="50" height="50">')
-    
-=======
             sort=Sort.NEWEST)
 
         df = pd.DataFrame(np.array(result), columns=['review'])
@@ -77,55 +46,14 @@ def index():
         df['User Image'] = df['User Image'].apply(
             lambda x: f'<img src="{x}" width="50" height="50">')
 
->>>>>>> 474c780 (InitialCommit)
         table_classes = 'table table-responsive table-stripped'
 
         df_html = df.to_html(index=False, escape=False, classes=table_classes)
 
-<<<<<<< HEAD
-        return render_template('index.html', table=df_html, package_name=package_name)
-    
-    else:
-        return render_template('index.html')
-
-@app.route("/api/predict", methods='POST')
-def prediction():
-    if request.method == "POST":
-        input_data = request.get_json()
-        text = input_data['text']
-        result = nlp('text')
-        return jsonify({
-            "status":{
-                "code":200,
-                "message": "Success predicting the sentiment"
-            }, "data": {
-                "sentiment": result}
-        }),200
-    else:
-        return "jsonify"({
-            "status":{
-                "code":405,
-                "message": "Method not allowed"
-            },"data" : None
-        }), 405
-
-if __name__ =="__main__":
-=======
         return render_template("index.html", table=df_html, package_name=package_name)
 
     else:
         return render_template("index.html")
-
-
-@app.route("/api/hello")
-def hello():
-    return jsonify({
-        "status": {
-            "code": 200,
-            "message": "Hello World!"
-        },
-        "data": None
-    }), 200
 
 
 @app.route("/api/predict", methods=["POST"])
@@ -156,5 +84,4 @@ def prediction():
 
 
 if __name__ == "__main__":
->>>>>>> 474c780 (InitialCommit)
     app.run()
