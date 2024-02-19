7b5f5d826a9a2bd3ff1ee7ca6bbaae9305e89cc0
bryanathll <bryannatanael77@gmail.com>
2024-02-19 17:18:53 +0700

    update flask app playstore crawler

--- src/utils/playstore.py
+++ src/utils/playstore.py
@@ -9,47 +9,47 @@ class Playstore:
             lang='id',
             country='id',
             sort=Sort.MOST_RELEVANT,
-            count=10
+            count=100
         )
 
         result = []
 
         for playstore_review in playstore_reviews:
-            result.appen({
+            result.append({
                 'user': playstore_review[ 'userName' ],
                 'review': playstore_review['content'],
                 'user_image': playstore_review['userImage'],
                 'score': playstore_review['score']
             })
 
-            # use ResultProcessor to proces the result into HTML
-            result_processor = ResultProcessor(result)
-            processed_result_html = result_processor.process_result()
+        # use ResultProcessor to proces the result into HTML
+        result_processor = ResultProcessor(result)
+        processed_result_html = result_processor.process_result()
 
-            return processed_result_html
+        return processed_result_html
         
-        class ResultProcessor:
-            def __init__(self, result):
-                self.reesult = result
+class ResultProcessor:
+    def __init__(self, result):
+        self.result = result
+        
+    def process_result(self):
+        # create a DataFrame from the 'result'
+        df = pd.DataFrame(self.result)
 
-            def process_result(self):
-                # create a DataFrame from the 'result'
-                df = pd.DataFrame(self.result)
+        # rename columns to match HTML table headers
+        df.rename(columns={'user': 'Username', 'review': 'Review', 
+                               'score': 'Rating', 'user_image': 'User Image'}, inplace = True)
+        # select only the desired columns
+        df = df[['User Image', 'Username', 'Review', 'Rating']]
 
-                # rename columns to match HTML table headers
-                df.rename(columns={'user': 'Username', 'review': 'Review', 
-                          'score': 'rating', 'user_image': 'User Image'}, inplace = True)
-                # select only the desired columns
-                df = df[['User Image', 'Username', 'Review', 'Rating']]
+        # add Tailwind CSS classes to the DataFrame
+        df['User Image'] = df['User Image'].apply(
+            lambda x: f'<img src ="{x}" width="50" height="50">')
 
-                # add Tailwind CSS classes to the DataFrame
-                df['User Image'] = df['User Image'].apply(
-                    lambda x: f'<img src ="{x}" width="50" height="50">')
+        # define table classes for styling
+        table_classes = 'table table-responsive table-striped'
 
-                # define table classes for styling
-                table_classes = 'table table-responsive table-striped'
+        # convert DataFrame to HTML
+        df_html = df.to_html(index=False, escape=False, classes=table_classes)
 
-                # convert DataFrame to HTML
-                df_html = df.to_HTML(index=False, escape=False, classes=table_classes)
-
-                return df_html
+        return df_html
